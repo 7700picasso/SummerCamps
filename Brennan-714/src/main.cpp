5b4fbb796476c4d2e24c532ecc0daaeb0f837856
7700R <94393717+7700picasso@users.noreply.github.com>
2025-07-18 16:07:53 -0700

    .

--- Brennan-714/src/main.cpp
+++ Brennan-714/src/main.cpp
@@ -8,6 +8,7 @@
 /*----------------------------------------------------------------------------*/
 
 #include "vex.h"
+#include <iostream>
 
 using namespace vex;
 
@@ -25,7 +26,8 @@ motor RB(PORT3, ratio18_1, false);
 
 inertial gyro1 = inertial(PORT19);
 
-void drivestop(){
+void drivestop()
+{
   LF.stop(brake);
   RF.stop(brake);
   LB.stop(brake);
@@ -40,52 +42,78 @@ void drive(int lspeed, int rspeed, int wt)
 
   wait(wt, msec);
 }
-int sign(float a){
-  if(a>0){
+int sign(float a)
+{
+  if (a > 0)
+  {
     return 1;
-  }else{
+  }
+  else
+  {
     return -1;
-  }  
+  }
 }
 
-float pi = 3.141592653589;
+float pi = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899;
 float dia = 4;
 float gearRatio = 1.5;
 
-void inchDrive(float target,float speed = 50){
-  LF.setPosition(0,rev);
-  float x = LF.position(rev)*dia*pi*gearRatio;
+void inchDrive(float target, float speed = 50)
+{
+  RF.setPosition(0, rev);
+  LF.setPosition(0, rev);
+  float x = LF.position(rev) * dia * pi * gearRatio;
   float error = target - x;
-  float kp = 3;
-  while(fabs(x)< fabs(target)){
-    x = LF.position(rev)*dia*pi*gearRatio;
+  float kp = 2.000000000000;
+  float kt = 0.01;
+  float initialAngle = gyro1.rotation();
+  while (fabs(x) < fabs(target))
+  {
+
+    float avgrev = (LF.position(rev) + RF.position(rev)) / 2;
+    x = avgrev * dia * pi * gearRatio;
     error = target - x;
-    drive(kp*error + 5*sign(error), kp*error + 5*sign(error), 10);
+    float turnError = initialAngle - gyro1.rotation();
+    float speed = kp * error + 5 * sign(error);
+    drive(speed * (1 + turnError * kt), speed * (1 - turnError * kt), 10);
   }
   drivestop();
 }
 
-void turn(float target){
+void turn(float target)
+{
   gyro1.resetRotation();
-  while(fabs(gyro1.rotation()) < fabs(target)){
-    drive(10*sign(target),-10*sign(target),10);
+  while (fabs(gyro1.rotation()) < fabs(target))
+  {
+    drive(10 * sign(target), -10 * sign(target), 10);
   }
   drivestop();
 }
 
-void turnTo(float target){
+void turnTo(float target)
+{
   float error = target - gyro1.yaw();
-  float kp = 0.5;
-  while(fabs(error)>1){
+  float prevError = error;
+  float kp = 0.75;
+  float kd = 9;
+  std::cout << "[";
+  float t = 0;
+  while (fabs(error) > 1)
+  {
     error = target - gyro1.yaw();
-    if(fabs(error)>180){
+    if (fabs(error) > 180)
+    {
       error = error - sign(error) * 360;
     }
-    drive(kp*error + 5*sign(error), -kp*error - 5*sign(error), 10);
+    float speed = kp * error + 5 * sign(error) + kd * (error - prevError);
+    prevError = error;
+    drive(speed, -speed, 10);
+    t += 0.01;
+    std::cout << ",(" << t << ", " << error << ")";
   }
+  std::cout << "]";
   drivestop();
 }
-
 
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
@@ -117,43 +145,35 @@ void pre_auton(void)
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
-void autonomous(void){
-  inchDrive(48);
-  drivestop();
-  turnTo(-45);
-  drivestop();
-  inchDrive(36);
-  drivestop();
-  turnTo(-90);
-  drivestop();
-  inchDrive(65);
-  drivestop();
+void autonomous(void)
+{
+  waitUntil(!gyro1.isCalibrating());
+  inchDrive(75);
+  inchDrive(-9);
+  turnTo(90);
+  wait(1, sec);
+  inchDrive(25);
+  turnTo(0);
+  inchDrive(60);
+  turnTo(270);
+  wait(1, sec);
+  inchDrive(30);
   turnTo(180);
-  drivestop();
-  inchDrive(84);
-  drivestop();
-  turnTo(-270);
-  drivestop();
-  inchDrive(84);
+  wait(1, sec);
+  inchDrive(20);
+  inchDrive(-24);
+  turnTo(45);
+  wait(1, sec);
+  inchDrive(42);
+  inchDrive(-30);
+  turnTo(135);
+  wait(1, sec);
+  inchDrive(23);
+  turnTo(45);
+  wait(1, sec);
+  drive(100, 100, 750);
   drivestop();
 
-
-  // inchDrive(72);
-  // drivestop();
-  // drive(-50, 50, 300);
-  // drivestop();
-  // inchDrive(84);
-  // drivestop();
-  // drive(-50, 50, 310);
-  // drivestop();
-  // inchDrive(72);
-  // drivestop();
-  // drive(-50, 50, 310);
-  // drivestop();
-  // inchDrive(84);
-  // drivestop();
-  
-  
   // ..........................................................................
   // Insert autonomous user code here.
   // ..........................................................................
@@ -199,60 +219,61 @@ void usercontrol(void)
       Brain.Screen.drawCircle(x, y, 10);
     }
 
-    cx+=Controller1.Axis4.position()/20;
-    cy-=Controller1.Axis3.position()/20;
-      
-      Brain.Screen.drawCircle(cx, cy, 10);
-      cx = fmax(0,cx);
-      cx = fmin(480,cx);
-      cy = fmax(0,cy);
-      cy = fmin(272,cy);
+    cx += Controller1.Axis4.position() / 20;
+    cy -= Controller1.Axis3.position() / 20;
 
-      if (Controller1.ButtonA.pressing()){
-      Brain.Screen.setFillColor(green);
-      }else
-      {
-        Brain.Screen.setFillColor(black);
-      }
-      int lstick = Controller1.Axis3.position();
-      int rstick = Controller1.Axis2.position();
-      drive(lstick,rstick,10);
+    Brain.Screen.drawCircle(cx, cy, 10);
+    cx = fmax(0, cx);
+    cx = fmin(480, cx);
+    cy = fmax(0, cy);
+    cy = fmin(272, cy);
 
-      Controller1.Screen.setCursor(0,0);
-      //gyro1.rotation();
-      //fyro1.heading();
-      Controller1.Screen.print("angle = %.2f     ",gyro1.rotation());
-
-      // This is the main execution loop for the user control program.
-      // Each time through the loop your program should update motor + servo
-      // values based on feedback from the joysticks.
-
-      // ........................................................................
-      // Insert user code here. This is where you use the joystick values to
-      // update your motors, etc.
-      // ........................................................................
-
-      wait(20, msec); // Sleep the task for a short amount of time to
-                      // prevent wasted resources.
-    }
-  }
-
-
-  //
-  // Main will set up the competition functions and callbacks.
-  //
-  int main()
-  {
-    // Set up callbacks for autonomous and driver control periods.
-    Competition.autonomous(autonomous);
-    Competition.drivercontrol(usercontrol);
-
-    // Run the pre-autonomous function.
-    pre_auton();
-
-    // Prevent main from exiting with an infinite loop.
-    while (true)
+    if (Controller1.ButtonA.pressing())
     {
-      wait(100, msec);
+      Brain.Screen.setFillColor(green);
     }
+    else
+    {
+      Brain.Screen.setFillColor(black);
+    }
+    int lstick = Controller1.Axis3.position();
+    int rstick = Controller1.Axis2.position();
+    drive(lstick*0.5, rstick*0.5,10);
+
+    Controller1.Screen.setCursor(0, 0);
+    // gyro1.rotation();
+    // fyro1.heading();
+    Controller1.Screen.print("angle = %.2f     ", gyro1.rotation());
+
+    // This is the main execution loop for the user control program.
+    // Each time through the loop your program should update motor + servo
+    // values based on feedback from the joysticks.
+
+    // ........................................................................
+    // Insert user code here. This is where you use the joystick values to
+    // update your motors, etc.
+    // ........................................................................
+
+    wait(20, msec); // Sleep the task for a short amount of time to
+                    // prevent wasted resources.
   }
+}
+
+//
+// Main will set up the competition functions and callbacks.
+//
+int main()
+{
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  // Run the pre-autonomous function.
+  pre_auton();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true)
+  {
+    wait(100, msec);
+  }
+}
