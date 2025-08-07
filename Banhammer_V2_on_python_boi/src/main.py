9f2fab70d4b8f65069cfa3d5dd668ac68db8f0d3
Max Babiec <94393717+7700picasso@users.noreply.github.com>
2025-08-07 12:06:15 -0700

    r

--- Banhammer_V2_on_python_boi/src/main.py
+++ Banhammer_V2_on_python_boi/src/main.py
@@ -19,7 +19,7 @@ RF = Motor(Ports.PORT9,GearSetting.RATIO_18_1,False)
 LB = Motor(Ports.PORT3,GearSetting.RATIO_18_1,True)
 RB = Motor(Ports.PORT11,GearSetting.RATIO_18_1,False)
 SP1 = Motor(Ports.PORT10,GearSetting.RATIO_6_1,False)
-
+gyro = Inertial(Ports.PORT20)
 
 def drive(left, right,wt):
     LF.spin(FORWARD,left,PERCENT)
@@ -48,6 +48,43 @@ def inchDrive(inches):
         brain.screen.print_at("inches = ",x,x=0,y=40)
     driveStop()
 
+def turnTo(angle):
+    error = angle - gyro.rotation()
+    kp = 5
+    timer = Timer()
+    while abs(error)>2 or timer.time(MSEC)>2000:
+        error = angle - gyro.rotation()
+        if error>180:
+            error = 360 -error
+        if error<-180:
+            error = 360 + error
+        brain.screen.print_at("error = ",error,x=0,y=40)
+        drive(kp*error,-kp*error,20)
+    brain.screen.print_at("done.              ",x=0,y=40)
+    driveStop()
+
+
+
+
+
+
+def autonomous():
+    gyro.calibrate()
+    while gyro.is_calibrating():
+        wait(20, MSEC)
+    inchDrive(24)
+    turnTo(90)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -66,47 +103,42 @@ x_value = 240
 y_value = 135
 t = 0
 circles =[[240,135]]
-while True: 
-    t+=0.05
-     #brain.screen.clear_screen()
-    if controller.buttonUp.pressing():
-        y_value-=5
-    if controller.buttonDown.pressing():
-        y_value+=5
-    if controller.buttonLeft.pressing():
-        x_value-=5
-    if controller.buttonRight.pressing():
-        x_value+=5
-     
-     #brain.screen.print_at("Touch at: ",x_value,y_value,x=x_value,y=y_value,sep = " ")
 
-    if(len(circles)>9):
-        circles =circles[1:9]
-    circles.append([x_value,y_value])
-
-
-    brain.screen.clear_screen()
-    for circle in circles:
-        x = circle[0]
-        y = circle[1]
-        if x<241 and y<137:
-            brain.screen.set_fill_color(Color.RED)
-        elif x>240 and y<137:
-            brain.screen.set_fill_color(Color.YELLOW)
-        elif x<241 and y>136:
-            brain.screen.set_fill_color(Color.BLUE)
-        else:
-            brain.screen.set_fill_color(Color.GREEN)
-        brain.screen.draw_circle(circle[0],circle[1],10) 
-    
-    left_stick = controller.axis3.position()
-    right_stick = controller.axis2.position()
-    drive(left_stick,right_stick,10)
-    wait(20,MSEC)
+def drivercontrol():
+    while True:
+        # t+=0.05
+        #brain.screen.clear_screen()
+        if controller.buttonUp.pressing():
+            y_value-=5
+        if controller.buttonDown.pressing():
+            y_value+=5
+        if controller.buttonLeft.pressing():
+            x_value-=5
+        if controller.buttonRight.pressing():
+            x_value+=5
+        
+        brain.screen.clear_screen()
+        for circle in circles:
+            x = circle[0]
+            y = circle[1]
+            if x<241 and y<137:
+                brain.screen.set_fill_color(Color.RED)
+            elif x>240 and y<137:
+                brain.screen.set_fill_color(Color.YELLOW)
+            elif x<241 and y>136:
+                brain.screen.set_fill_color(Color.BLUE)
+            else:
+                brain.screen.set_fill_color(Color.GREEN)
+            brain.screen.draw_circle(circle[0],circle[1],10)
+        
+        left_stick = controller.axis3.position()
+        right_stick = controller.axis2.position()
+        drive(left_stick,right_stick,10)
+        wait(20,MSEC)
 
 
 
-
+comp = Competition(drivercontrol,autonomous) 
 
 
 
