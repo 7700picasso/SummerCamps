47495c3086ed49a135c4e57ce2ccfc5fb9b14413
7700R <94393717+7700picasso@users.noreply.github.com>
2025-08-08 09:06:25 -0700

    code

--- JackCompCode2/src/main.py
+++ JackCompCode2/src/main.py
@@ -1,3 +1,4 @@
+
 # ---------------------------------------------------------------------------- #
 #                                                                              #
 # 	Module:       main.py                                                      #
@@ -12,11 +13,12 @@ from vex import *
 
 brain = Brain()
 
-#Mortor labels
+#Labels
 lf = Motor(Ports.PORT14, GearSetting.RATIO_18_1, True)
 lb = Motor(Ports.PORT12, GearSetting.RATIO_18_1, True)
 rf = Motor(Ports.PORT8, GearSetting.RATIO_18_1, False)
 rb = Motor(Ports.PORT16, GearSetting.RATIO_18_1, False)
+gyro = Inertial(Ports.PORT4)
 
 pi_num = 3.14
 dia = 4.0
@@ -58,38 +60,82 @@ def inch_drive(target):
         #stop
         brake()
 
+def gyro_print():
+    rotation = gyro.rotation(DEGREES)
+    brain.screen.print_at("rotations:", rotation, x = 0, y = 80)
+
+def gyro_turn(target):
+    rotation = 0.0
+    gyro.reset_rotation()
+    error = target - rotation
+    accuracy = 5.0
+    kp = 1.3
+    speed = kp * error
+    # kp reduces the speed of the rotation as the rotation nears the target
+    while abs(error) >= accuracy:
+        gyro_print()
+        drive(speed, -speed, 10)
+        rotation = gyro.rotation(DEGREES)
+        error = target - rotation
+        speed = kp * error
+        brake()
+
+
+
 def autonomous():
     brain.screen.clear_screen()
     brain.screen.print("autonomous code")
-    # # place automonous code here
-    inch_drive(24)
+   
+    inch_drive(120)
     brake()
-    drive(25,-25, 600)
+    inch_drive(-12)
     brake()
-    inch_drive(20)
+    gyro_turn(-90)
     brake()
-    drive(25, -25,450)
+    inch_drive(84)
     brake()
-    inch_drive(10)
+    gyro_turn(-90)
     brake()
-    drive(-25, 25, 450)
+    inch_drive(48)
     brake()
-    inch_drive(15)
+    gyro_turn(-90)
     brake()
-    drive(-25, 25, 350)
+    inch_drive(36)
     brake()
+    inch_drive(-36)
+
+
+
+    # place automonous code here
+   
+    # inch_drive(24)
+    # brake()
+    # drive(25,-25, 600)
+    # brake()
+    # inch_drive(20)
+    # brake()
+    # drive(25, -25,450)
+    # brake()
+    # inch_drive(10)
+    # brake()
+    # drive(-25, 25, 450)
+    # brake()
+    # inch_drive(16)
+    # brake()
+    # drive(-25, 25, 350)
+    # brake()
 
 def user_control():
     brain.screen.clear_screen()
     brain.screen.print("driver control")
     # place driver control in this while loop
+    gyro.calibrate()
     while True:
+        gyro_print()
         wait(20, MSEC)
-
 
 
 # create competition instance
 comp = Competition(user_control, autonomous)
-
 # actions to do when the program starts
 brain.screen.clear_screen()
