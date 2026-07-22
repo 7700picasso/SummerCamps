cf610e8ee262a7fb6d7992b001751ce7af7bc951
7700R <94393717+7700picasso@users.noreply.github.com>
2026-07-22 16:10:48 -0700

    dariush

--- Dariush67C/src/main.cpp
+++ Dariush67C/src/main.cpp
@@ -15,22 +15,55 @@ using namespace vex;
 competition Competition;
 
 // define your global instances of motors and other devices here
+
+vex::brain Brain;
 motor LM = motor(PORT1, ratio18_1, false);
 motor RM = motor(PORT19, ratio18_1, true);
-
-
+motor BLM = motor(PORT11, ratio18_1, false);
+motor BRM = motor(PORT20, ratio18_1, true);
+double pi = 3.14159265;
+float dia = 4.0;
+float gearRatio = 60.0/48.0;
 //movement functions here
 
 void drive(int Lspeed, int Rspeed, int wt){
   LM.spin(forward, Lspeed, pct);
   RM.spin(forward, Rspeed, pct);
+  BLM.spin(forward, Lspeed, pct);
+  BRM.spin(forward, Rspeed, pct);
   wait(wt, msec);
 }
 
 void driveBrake(){
   LM.stop(brake);
   RM.stop(brake);
+  BLM.stop(brake);
+  BRM.stop(brake);
 }
+
+
+void inchDrive(float target, float speed){
+  float x = 0;
+  LM.setPosition(0, rev);
+  x = LM.position(rev)*dia*pi*gearRatio;
+  if (x >= 0){
+    while(x < fabs(target)){
+      drive(speed, speed, 10);
+      x = LM.position(rev)*dia*pi*gearRatio;
+      Brain.Screen.printAt(10, 20, "inches = %0.2f", x);
+    }
+  }
+  else{
+    while(x > fabs(target)){
+      drive(speed*-1, speed*-1, 10);
+      x = LM.position(rev)*dia*pi*gearRatio;
+      Brain.Screen.printAt(10, 20, "inches = %0.2f", x);
+    }
+  driveBrake();
+  }
+}
+
+
 
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
@@ -59,14 +92,54 @@ void pre_auton(void) {
 /*---------------------------------------------------------------------------*/
 
 void autonomous(void) {
-  while(true){
-  drive(50, 50, 1000);
-  drive(-50, -50, 1000);
+  inchDrive(20, 25);
+  wait(300, msec);
+  drive(-25, 25, 740);
+  driveBrake();
+  wait(300, msec);
+  inchDrive(17, 25);
+  wait(300, msec);
+  drive(-25, 25, 720);
+  driveBrake();
+  inchDrive(-40, 25);
   }
+  // drive(50, 50, 1000);
+  // driveBrake();
+  // wait(500, msec);
+  // drive(50, -50, 710);
+  // driveBrake();
+  // wait(500, msec);
+  // drive(-50, -50, 1100);
+  // driveBrake();
+  // wait(500, msec);
+  // drive(-50, 50, 670);
+  // driveBrake();
+  // wait(500, msec);
+  // drive(-50, -50, 1300);
+  // driveBrake();
+  // wait(500, msec);
+  // drive(50, -50, 710);
+  // driveBrake();
+  // wait(500, msec);
+  // drive(-50, -50, 1000);
+  // driveBrake();
+  // wait(500, msec);
+  // drive(50, -50, 710);
+  // driveBrake();
+  // wait(500, msec);
+  // drive(-50, -50, 900);
+  // //Halfway
+  // driveBrake();
+  // wait(500, msec);
+  // drive(50, 50, 900);
+  // driveBrake();
+  // wait(500, msec);
+  // drive(50, -50, 710);
+  // driveBrake();
+  // wait(500, msec);
   // ..........................................................................
   // Insert autonomous user code here.
   // ..........................................................................
-}
 
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
